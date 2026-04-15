6b1f041e66c0938aebadeb94e2dd33e77981dc30
Njabulo <32726834+twiceTheProgrammer@users.noreply.github.com>
2026-04-15 16:46:52 +0200

    .gitignore binaries

--- Calculator/obj/Debug/net10.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net10.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8926c20dc1e997f23f62accff1239abc2a195c31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+178ba3e3775c55fa5390f52b0871d7a801c1f06f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
