b5de249f0499d7e9091a375f8c82d168deaf3709
Njabulo <njabulo.magagula@rib-software.com>
2026-05-07 17:27:07 +0200

    NavigationView: + icons

--- Horizon/res/navigation/navigation.js
+++ Horizon/res/navigation/navigation.js
@@ -7,9 +7,22 @@ export class NavigationView extends Element
 				<h2>Horizon Hub</h2>
 			</header>
 			<ul #navigation-bar >
-				<li checked value="tasks">Tasks</li>
-				<li  value="inventory">My Inventory</li>
-				<li value="reports">Reports</li>
+				<li #tasks      value="tasks" checked >My Tasks</li>
+				<li #inventory  value="inventory">My Inventory</li>
+				<li #reports    value="reports">Reports</li>
+				<hr/>
+				<li #estimating         value="estimating">Estimating</li>
+				<li #planning           value="planning">Planning</li>
+				<li #forecasting        value="forecasting">Forecasting</li>
+				<li #cashflow           value="cashflow">Cashflow</li>
+				<li #valuations         value="valuations">Valuations</li>
+				<li #subcontractManager value="subcontract">Subcontract Manager</li>
+				<li #costAndAllowables  value="costAndAllowables">Cost & Allowables</li>
+				<li #materials          value="materials">Materials Received</li>
+				<li #drawings           value="drawings">Drawings</li>
+				<hr/>
+				<li #profile  value="profile">Profile</li>
+				<li #settings value="settings">Settings</li>
 			</ul>
 		</section>;
 	}
