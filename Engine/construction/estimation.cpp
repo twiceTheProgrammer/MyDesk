1432e746baf9bcfdc745e75e57c0b1ea700f900a
Njabulo <njabulozweli08@gmail.com>
2026-05-13 15:40:46 +0200

    + test basic Est report

--- Engine/construction/estimation.cpp
+++ Engine/construction/estimation.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 Estimate::Estimate() {}
 
-int Estimate::NumberOfBricks(int cements){
+int Estimate::BricksProduced(int cements){
 	int bricks = cements * BRICKS_PER_BAG;
 	return bricks;
 }
@@ -15,38 +15,55 @@ int Estimate::SandRequired(int cement)
 	return cement * WHEELBARROWS_PER_BAG;
 }
 
-int Estimate::BricksForWall(double length, double width, double height)
+int Estimate::BricksForWall(int length, int width, int height)
 {
-	double wallArea = 2 * ((length * height) + (width * height));  // perimeter walls
-	return static_cast<int>(wallArea * BRICKS_PER_M2 );
+	double wallArea = 2 * (length * height + width * height);  // perimeter walls
+	double brickFaceArea = BRICK_LENGTH * BRICK_HEIGHT;
+	return static_cast<int>(wallArea / brickFaceArea );
+}
+
+int Estimate::CementRequiredForWall(int bricks) {
+	return (bricks + 25 - 1) / 25;   // ~25 blocks per bag of cement
 }
 
 int main()
 {
+	Estimate estimate;
 	int bagsOfCement;
 	int sand;
 	int length, width, height;
 
 	cout << "===| HORIZON HUB ESTIMATION |===\n" << endl;
 	
-	cout << "Enter septic tank length (m) : " << endl;
+	cout << "Septic tank dimensions" << endl;
+	cout << "Enter length (m) : " << endl;
 	cin >> length;
 
-	cout << "Enter septic tank width (m) : " << endl;
+	cout << "Enter width (m) : " << endl;
 	cin >> width;
 
-	cout << "Enter septic tank height (m) : " << endl;
+	cout << "Enter height (m) : " << endl;
 	cin >> height;
 
+	
+	cout << "\nBricks Production" << endl;
 	cout << "Enter cement bags : " << endl;
 	cin >> bagsOfCement;
 
-	int bricks = estimate.BricksForWall(length, width, height);
-	
+	// Bricks Production.
+	int producedBricks = estimate.BricksProduced(bagsOfCement);
+	int sandForProduction = estimate.SandRequired(bagsOfCement);
+	int bricksForWall = estimate.BricksForWall(length, width, height);
+	int cementForWall = estimate.CementRequiredForWall(bricksForWall);
+
 	cout << "\n--- Estimation Report -----\n";
-	cout << "Wall Area : " << 2 * (length * height + width * height) << " m2"  << endl;
-	cout << "Bricks required : " << bricks << endl;
-	cout << "Sand Required : " << estimate.SandRequired(bagsOfCement) << " Wheel barrows"<<endl;
+	cout << "Brick Production:\n";
+	cout << " Bricks produced : " << producedBricks << endl;
+	cout << " Sand Required : " << sandForProduction << " Wheel barrows"<<endl;
+
+	cout << "\nWall construction : " << endl;
+	cout << " Bricks Needed : " << bricksForWall << endl;
+	cout << "Cement Required : " <<  cementForWall << endl;
 
 	return 0;
 }
--- Engine/construction/estimation.h
+++ Engine/construction/estimation.h
@@ -3,18 +3,20 @@
 class Estimate 
 {
 	private: 
-		int const BRICKS_PER_BAG = 50;
-		int const BRICKS_PER_M2 = 50;   // ~50 brick per square meter
+		int const BRICKS_PER_BAG = 50;  // production ratio
+		double const BRICK_LENGTH = 0.39;   // meters
+		double const BRICK_HEIGHT = 0.19;   // meters
 		int const WHEELBARROWS_PER_BAG = 4; // let's make our ratio 1 bag for every 4 wheel barrows of sand.
 
 	public:
 		Estimate();
-		int NumberOfBricks(int cements);
+		int BricksProduced(int cements);
 		int SandRequired(int bagsOfCement);
-		int BricksForWall(double length, double height, double width);
+		int BricksForWall(int length, int height, int width);
+		int CementRequiredForWall(int bricks);  // mortar requirement
 };
 
 
 Estimate estimate;
-int EstimateBricks(int cementBags) { return estimate.NumberOfBricks(cementBags); }
+int EstimateBricks(int cementBags) { return estimate.BricksProduced(cementBags); }
 int EstimateSand(int cement) { return estimate.SandRequired(cement); }
